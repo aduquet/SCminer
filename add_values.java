6b5bd341458fdb6512997ee4c12e65bc33dca493
aduquet <duquet@ut.ee>
2021-11-09 18:34:21 +0100

    files for testing stuff

---
+++ add_values.java
@@ -0,0 +1,19 @@
+public class add_values {
+    public static int add_values_m(Integer[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ add_values.py
@@ -0,0 +1,5 @@
+def add_values(a):
+    sum = 0
+    for i in a:
+        sum += i
+    return sum

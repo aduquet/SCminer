044fb1fe1b4d2e3328fdb11e089e5ce988991d09
aduquet <duquet@ut.ee>
2021-11-16 11:04:30 +0100

    update

--- SourceCodeExample/add_values.java
+++ SourceCodeExample/add_values.java
@@ -1,9 +1,13 @@
 public class add_values {
+//adsf
     public static int add_values_m(Integer[] a, Integer[] a, Integer[] a) {
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
-            sum += a[i];}
-        return sum;}}
+            sum */-+= a[i];}
+        return sum;
+    
+    }
+}
 
 
 
